6c0ebd296ae597b4516cd975b14b113d7384883e
MahmoudGomaa167 <83142218+MahmoudGomaa167@users.noreply.github.com>
2021-08-19 21:28:09 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-    if($('.home-page').length > 0){
+$(document).ready(function () {
+    if ($('.home-page').length > 0) {
         let goTopBtn = $('#goTop');
         let homeOffset = Math.ceil($('#home').offset().top);
 
         // Navbar chnage backgroundColor & padding on scroll 
-        $(window).scroll(function (){
+        $(window).scroll(function () {
             let windowScroll = Math.ceil($(window).scrollTop());
-            
-            if(windowScroll >= 50){
+
+            if (windowScroll >= 50) {
                 $('nav').css({
                     backgroundColor: 'rgba(0,0,0,0.8)',
                     padding: '15px 0'
                 });
             }
-            else{
+            else {
                 $('nav').css({
                     backgroundColor: 'transparent',
                     padding: '30px 0'
@@ -22,28 +22,28 @@ $(document).ready(function(){
         });
 
         // Add active class to links on click
-        $('.list-link').click(function(){
+        $('.list-link').click(function () {
             let sectionHref = $(this).attr('href');
             let sectionOffset = $(sectionHref).offset().top;
 
-           $('body, html').animate({
-               scrollTop: sectionOffset - 50
-           }, 1500);
+            $('body, html').animate({
+                scrollTop: sectionOffset - 50
+            }, 1500);
 
             $(this).addClass('active');
             $(this).parent().siblings().children().removeClass('active')
-            
+
         });
 
         // Add active class to links on scroll
         $(window).scroll(function () {
             let windowScroll = Math.ceil($(window).scrollTop());
             let links = $('.list-link');
-            
-            for(let link of links){
+
+            for (let link of links) {
                 let sectionHref = $(link).attr('href');
                 let sectionOffset = Math.ceil($(sectionHref).offset().top);
-                if(windowScroll >= sectionOffset - 100){
+                if (windowScroll >= sectionOffset - 100) {
                     $(link).addClass('active');
                     $(link).parent().siblings().children().removeClass('active')
                 }
@@ -51,15 +51,15 @@ $(document).ready(function(){
         })
 
         // Show go up button on scroll
-        $(window).scroll(function(){
+        $(window).scroll(function () {
             let windowScroll = Math.ceil($(window).scrollTop());
             let homeHeight = $('#home').height();
 
-            if(windowScroll >= homeHeight - 100){
+            if (windowScroll >= homeHeight - 100) {
                 $(goTopBtn).css('opacity', '1');
-            }else{
+            } else {
                 $(goTopBtn).css('opacity', '0');
-                
+
             }
         });
 
@@ -77,14 +77,14 @@ $(document).ready(function(){
             let servicesOffset = Math.ceil($('#services').offset().top);
             let progressBar = document.querySelectorAll('.custom-progress');
             let width;
-            
-            for(let element of progressBar){
+
+            for (let element of progressBar) {
                 width = element.getAttribute('data-width');
-                if(windowScroll >= aboutOffset - 100 && windowScroll < servicesOffset - 100){
+                if (windowScroll >= aboutOffset - 100 && windowScroll < servicesOffset - 100) {
                     $(element).css(
-                        'width' , `${width}%`
+                        'width', `${width}%`
                     )
-                }else{
+                } else {
                     $(element).css('width', '0')
                 }
             }
@@ -93,34 +93,34 @@ $(document).ready(function(){
         //counter
         let counters = document.querySelectorAll('.counter-number');
 
-        let countUp = function(){
+        let countUp = function () {
             counters.forEach((counter) => {
-                $(window).scroll(function(){
+                $(window).scroll(function () {
                     let windowScroll = Math.ceil($(window).scrollTop()),
                         counterOffset = Math.ceil($('.counter').offset().top);
-                        workOffset = Math.ceil($('#portfolio').offset().top);
+                    workOffset = Math.ceil($('#portfolio').offset().top);
 
 
-                    
-                    if(windowScroll >= counterOffset - 300 && windowScroll <= workOffset - 100){
-                        function updateCounter (){
+
+                    if (windowScroll >= counterOffset - 300 && windowScroll <= workOffset - 100) {
+                        function updateCounter() {
                             let target = Number(counter.getAttribute('data-target'));
                             let c = Number(counter.innerText);
-                            let increment = target / 200;
-                            if (c < target){
+                            let increment = target / 500;
+                            if (c < target) {
                                 counter.innerText = `${Math.ceil(c + increment)}`;
                                 setInterval(updateCounter, 1)
-                            }else{
+                            } else {
                                 counter.innerText = target;
-                                
+
                             }
                         }
                         updateCounter();
                     }
-        
+
                 })
-                
-            }); 
+
+            });
         }
         countUp();
 
@@ -134,8 +134,8 @@ $(document).ready(function(){
         let index;
 
 
-        for(let image of images){
-            $(image).click(function (){
+        for (let image of images) {
+            $(image).click(function () {
                 let imageSrc = $(this).attr('src');
                 index = imagesArr.indexOf(image);
 
@@ -147,27 +147,27 @@ $(document).ready(function(){
             })
         }
 
-        function next(){
+        function next() {
             index++;
-            if(index > imagesArr.length - 1){
+            if (index > imagesArr.length - 1) {
                 index = 0;
                 $(modalImage).attr('src', imagesArr[0].getAttribute('src'));
-            }else{
+            } else {
                 $(modalImage).attr('src', imagesArr[index].getAttribute('src'));
             }
         }
 
-        function prev(){
+        function prev() {
             index--;
-            if(index < 0){
+            if (index < 0) {
                 index = imagesArr.length - 1;
                 $(modalImage).attr('src', imagesArr[index].getAttribute('src'));
-            }else{
+            } else {
                 $(modalImage).attr('src', imagesArr[index].getAttribute('src'));
             }
         }
 
-        function close(){
+        function close() {
             $('.portfolio-modal').css('display', 'none');
             $('body').css('overflow', 'auto');
         }
@@ -178,38 +178,48 @@ $(document).ready(function(){
 
         $(closeBtn).click(close);
 
-       $(document).keyup(function (event){
-           let key = event.code;
-           if($('.portfolio-modal').css('display') !== 'none'){
-               if(key === 'ArrowRight'){
-                   next();
-               }
-               if(key === 'ArrowLeft'){
-                   prev();
-               }
-               if(key === 'Escape'){
-                   close();
-               }
-           }
-       });
-        
-
-        $('.testimonial-item').owlCarousel({
-            loop:true,
-            margin:0,
-            nav:false,
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:1
-                },
-                1000:{
-                    items:1
+        $(document).keyup(function (event) {
+            let key = event.code;
+            if ($('.portfolio-modal').css('display') !== 'none') {
+                if (key === 'ArrowRight') {
+                    next();
+                }
+                if (key === 'ArrowLeft') {
+                    prev();
+                }
+                if (key === 'Escape') {
+                    close();
                 }
             }
         });
-    } 
+
+        // Menu bars
+        $('#menuicon').click(function () {
+            $(this).toggleClass('icon-close');
+            $(this).toggleClass('icon-bars');
+            $('#custom-navbar').toggleClass('mobile-navbar');
+            $('#custom-navbar').toggleClass('custom-navbar');
+            $('.dropdown-list').toggleClass('mobile');
+        });
+
+
+        // Testimonials Owl Carousel
+        $('.testimonial-item').owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
+    }
 })
 
